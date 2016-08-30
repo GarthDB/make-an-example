b463639ff51c50692cdd482ce3fd89b6d5965c73
garthdb <garthdb@gmail.com>
2016-08-29 19:39:47 -0600

    updated gitignore

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,11 @@
 import test from 'ava';
-import MakeAnExample, {
+import fs from 'fs';
+import makeAnExample, {
   getArgsNames,
   getFuncName,
   generateArgsObj,
   generateFunctionObj,
-  formatFunction,
+  formatExample,
 } from '../src/';
 
 test('Get argument names', t => {
@@ -25,40 +26,26 @@ test('Get function name', t => {
   t.deepEqual(result, expected);
 });
 
-test('Function name false when anonymous', t => {
-  let result;
-  const expected = false;
-  const aFunc = function (arg1, arg2) {
-    result = getFuncName(arguments.callee);
-    t.deepEqual(result, expected);
-    return [arg1, arg2];
-  };
-  aFunc('value 1', 'value 2');
-});
-
 test('Generate Arguments Object', t => {
-  let args;
+  // eslint-disable-next-line no-unused-vars
   function aFunc(arg1, arg2) {
     // eslint-disable-next-line prefer-rest-params
-    args = arguments;
-    return [arg1, arg2];
+    return arguments;
   }
-  aFunc('value 1', 'value 2');
-  const argNames = getArgsNames(aFunc);
-  const argValues = Array.prototype.slice.call(args);
-  const argsObj = generateArgsObj(argNames, argValues);
+  const args = aFunc('value 1', 'value 2');
+  const argNames = getArgsNames(aFunc.toString());
+  const argsObj = generateArgsObj(argNames, args);
   const expected = { arg1: 'value 1', arg2: 'value 2' };
   t.deepEqual(argsObj.args, expected);
 });
 
 test('Generate Arguements Object with extra arguments', t => {
-  let args;
+  // eslint-disable-next-line no-unused-vars
   function aFunc(arg1, arg2) {
     // eslint-disable-next-line prefer-rest-params
-    args = arguments;
-    return [arg1, arg2];
+    return arguments;
   }
-  aFunc('value 1', 'value 2', 'extra 1', 'extra 2');
+  const args = aFunc('value 1', 'value 2', 'extra 1', 'extra 2');
   const argNames = getArgsNames(aFunc);
   const argValues = Array.prototype.slice.call(args);
   const argsObj = generateArgsObj(argNames, argValues);
@@ -70,21 +57,58 @@ test('Generate Arguements Object with extra arguments', t => {
 });
 
 test('Generate Function Object', t => {
-  let args;
+  // eslint-disable-next-line no-unused-vars
   function aFunc(arg1, arg2) {
     // eslint-disable-next-line prefer-rest-params
-    args = arguments;
-    return [arg1, arg2];
+    return arguments;
   }
-  aFunc('value 1', 'value 2');
-  const funcName = args.callee.name;
+  const args = aFunc('value 1', 'value 2');
+  const funcName = getFuncName(aFunc);
   const argNames = getArgsNames(aFunc);
   const argValues = Array.prototype.slice.call(args);
   const argsObj = generateArgsObj(argNames, argValues);
   const funcObj = generateFunctionObj(funcName, argsObj);
   const expected = {
+    name: 'aFunc',
     args: { arg1: 'value 1', arg2: 'value 2' },
-    extraArgs: ['extra 1', 'extra 2'],
   };
   t.deepEqual(funcObj, expected);
+});
+
+test('Format Example', t => {
+  let result = '';
+  function format(message) {
+    result += message;
+    result += '\n';
+  }
+  // eslint-disable-next-line no-unused-vars
+  function aFunc(arg1, arg2) {
+    // eslint-disable-next-line prefer-rest-params
+    return arguments;
+  }
+  const args = aFunc('value 1', 'value 2', 'extra 1', 'extra 2');
+  const funcName = getFuncName(aFunc);
+  const argNames = getArgsNames(aFunc);
+  const argValues = Array.prototype.slice.call(args);
+  const argsObj = generateArgsObj(argNames, argValues);
+  const funcObj = generateFunctionObj(funcName, argsObj);
+  formatExample(funcObj, format);
+  const expected = fs.readFileSync('./expected/formatted-message.txt', 'utf-8');
+  t.deepEqual(result, expected);
+});
+test('Format Example from MakeAnExample', t => {
+  let result = '';
+  function format(message) {
+    result += message;
+    result += '\n';
+  }
+  // eslint-disable-next-line no-unused-vars
+  function aFunc(arg1, arg2) {
+    // eslint-disable-next-line prefer-rest-params
+    return arguments;
+  }
+  const args = aFunc('value 1', 'value 2', 'extra 1', 'extra 2');
+  makeAnExample(aFunc, args, formatExample, format);
+  const expected = fs.readFileSync('./expected/formatted-message.txt', 'utf-8');
+  t.deepEqual(result, expected);
 });
